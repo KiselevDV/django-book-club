2450c762251a88356881e13981a360cb0edc7a7a
Dmitry <kiselevdv1991@gmail.com>
2020-09-28 20:06:32 +0300

    fix: make edits to the application model

--- book_club/books/models.py
+++ book_club/books/models.py
@@ -114,7 +114,7 @@ class Rating(models.Model):
     """Рейтинг"""
     ip = models.CharField("IP адрес", max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
-    book = models.ForeignKey(Book, on_delete=models.CharField, verbose_name="фильм")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="фильм")
 
     def __str__(self):
         return f'{self.star} - {self.book}'
