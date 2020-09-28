ea18ece7d093520662c25da03417595718911ff5
Dmitry <kiselevdv1991@gmail.com>
2020-09-28 20:10:29 +0300

    fix: make edits to the application model

--- book_club/books/admin.py
+++ book_club/books/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from .models import Category, Genre, Topic, Author, Book, BookExcerpt, RatingStar, Rating, Reviews
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Topic)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookExcerpt)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Reviews)
 
 # Описываем модели которые хотим зарегистрировать и выводить в админке
 # Register your models here.
--- book_club/books/models.py
+++ book_club/books/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     """Категории"""
-    name = models.CharField("Категория", max_length=30)
+    name = models.CharField("Тема", max_length=30)
     description = models.CharField("Описание", max_length=300)
     url = models.SlugField(max_length=120, unique=True)
 
@@ -13,8 +13,8 @@ class Category(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+        verbose_name = "Тема"
+        verbose_name_plural = "Темы"
 
 
 class Genre(models.Model):
@@ -29,6 +29,20 @@ class Genre(models.Model):
     class Meta:
         verbose_name = "Жанр"
         verbose_name_plural = "Жанры"
+
+
+class Topic(models.Model):
+    """Темы/подборки"""
+    name = models.CharField("Название", max_length=150)
+    description = models.TextField("Описание", blank=True, null=True)
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тема"
+        verbose_name_plural = "Темы"
 
 
 class Author(models.Model):
@@ -67,6 +81,7 @@ class Book(models.Model):
         Author, verbose_name="Иллюстратор", related_name="book_illustrator", blank=True)
     publisher = models.CharField("Издательство", max_length=80)
     genres = models.ManyToManyField(Genre, verbose_name="Жанры")
+    topics = models.ManyToManyField(Topic, verbose_name="Темы")
     category = models.ForeignKey(
         Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
     age = models.CharField("Возрастной ценз", max_length=3,
